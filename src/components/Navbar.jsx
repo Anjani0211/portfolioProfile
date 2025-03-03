ca9590f1ba3b2f3bc7376d9bae6f9765d24c65d1
anjani <anjani.kumar@gmail.com>
2025-03-03 23:59:37 +0530

    #

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         <Link to="/">Anjani Kumar</Link></p>
       <nav className="chakra-petch-medium  p-4 bg-white text-black border-1 border-gray-200 rounded-full shadow">
         <div className="flex md:justify-between justify-center  px-4">
-          <a href="#about" className="chakra-petch-bold text-xl text-orange-600 hidden md:block">Anjani kumar</a>
+          <Link to="/" className="chakra-petch-bold text-xl text-orange-600 hidden md:block">Anjani kumar</Link>
 
           <ul className="flex justify-evenly gap-4">
             <li>
